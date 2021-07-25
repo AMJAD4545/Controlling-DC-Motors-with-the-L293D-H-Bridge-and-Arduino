a5a425bbccf39f01b0cdbb846f676d4dda4d02a4
امجاد مطلق المطيري <85800978+AMJAD4545@users.noreply.github.com>
2021-07-25 07:23:22 +0300

    Create Coding.ino

---
+++ Coding.ino
@@ -0,0 +1,24 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(7, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(13, HIGH);
+  digitalWrite(12, LOW);
+  digitalWrite(8, HIGH);
+  digitalWrite(7, LOW);
+  delay(2000); // Wait for 1000 millisecond(s)
+  digitalWrite(13, LOW);
+  digitalWrite(12, HIGH);
+  digitalWrite(8, LOW);
+  digitalWrite(7, HIGH);
+  delay(2000); // Wait for 1000 millisecond(s)
+ 
+}
